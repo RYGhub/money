20850eca3f725b2cfa7469848dc0d267d5c1bdef
Nemesis <lorenzo.balugani@gmail.com>
2022-01-31 18:45:25 +0100

    Remove useless evals

--- auto.sh
+++ auto.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-eval "git pull"
-eval "chmod 444 ryg.beancount"
-eval "chmod 700 auto.sh"
-eval "./venv/bin/python -m fava.cli -p 30080 ryg.beancount"
+
+git pull
+chmod 444 ryg.beancount
+chmod 700 auto.sh
+./venv/bin/python -m fava.cli -p 30080 ryg.beancount
