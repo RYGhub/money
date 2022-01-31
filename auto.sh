780e66cf2146564a150b6d40d7f1b1487395684d
Nemesis <lorenzo.balugani@gmail.com>
2022-01-31 18:25:52 +0100

    Set custom port for fava webserver

--- auto.sh
+++ auto.sh
@@ -2,4 +2,4 @@
 eval "git pull"
 eval "chmod 444 ryg.beancount"
 eval "chmod 700 auto.sh"
-eval "./venv/bin/python -m fava.cli ryg.beancount"
+eval "./venv/bin/python -m fava.cli -p 30080 ryg.beancount"
